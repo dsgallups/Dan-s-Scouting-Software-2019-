91ae61d277753e75828a804bc2fa62e6d5b2e1cb
Daniel Gallups <dgallups@purdue.edu>
2019-02-28 17:28:42 -0500

    server.js complete

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ mongoose.connect('mongodb://128.211.235.107/frc')
  *  {
  *      initials: {
  *          team_id: <team_no>,
+ *          match_no: <match_no>,
  *          pos: ['<L/M/R>','<T/B>',
  *          gamepiece: '<hatch/cargo>',
  *          preloaded: [
@@ -76,6 +77,7 @@ mongoose.connect('mongodb://128.211.235.107/frc')
 const rSchema = new mongoose.Schema({
     initials: {
         team_id: Number,
+        match_no: Number,
         pos: [String],
         gamepiece: String,
         preloaded: []
@@ -101,32 +103,9 @@ const TeamMatch = mongoose.model('TeamMatch', rSchema);
 //Now we need to handle POSTs:
 app.post('/frc', (req, res) => {
 	//This is super annoying. We have to declare schemas for children objects. smh.
-	/*
-    const mainSchema = {
-        initals: {
-            team_id: Joi.number().integer(),
-            pos: Joi.array().items(Joi.string()),
-            gamepiece: Joi.string().valid('hatch','cargo'),
-            preloaded: Joi.array()
-        },
-        sandstorm: {
-            autonomous: Joi.boolean(),
-            hit_1747: Joi.boolean(),
-            points: Joi.array()
-        },
-        teleop: {
-            points: Joi.array()
-        },
-        endgame: {
-            climb_level: Joi.number().min(0).max(3),
-            buddy_climb: Joi.array(),
-            win: Joi.boolean()
-        },
-        notes: Joi.string()
-    };
-	*/
 	const iSchema = Joi.object({
             team_id: Joi.number().integer(),
+            match_no: Joi.number().integer().min(0),
             pos: Joi.array().items(Joi.string()),
             gamepiece: Joi.string().valid('hatch','cargo'),
             preloaded: Joi.array()
@@ -145,14 +124,14 @@ app.post('/frc', (req, res) => {
             win: Joi.boolean()
 	});
 	const nSchema = Joi.string();
-	
-    const mainSchema = {
-        initals: iSchema,
+
+    const mainSchema = Joi.object({
+        initials: iSchema,
         sandstorm: sSchema,
         teleop: tSchema,
         endgame: eSchema,
         notes: nSchema
-    };
+    });
     //Based on the schema above, we'll compare whatever JSON was posted to us with that and store the returned JSON in val
     const result = Joi.validate(req.body, mainSchema);
 
@@ -167,6 +146,7 @@ app.post('/frc', (req, res) => {
     const teamMatch = new TeamMatch({
         initials: {
             team_id: o.initials.team_id,
+            match_no: o.initials.match_no,
             pos: o.initials.pos,
             gamepiece: o.initials.gamepiece,
             preloaded: o.initials.preloaded
@@ -190,10 +170,51 @@ app.post('/frc', (req, res) => {
     //Now our obj is in teamMatch, lets send it to the DB.
     async function sendTeamMatch() {
         const result = await teamMatch.save();
-        console.log(result);
+        console.log("request responded with result.");
         res.send(result)
     }
     sendTeamMatch();
+});
+
+//Fetch all records for one team
+app.get('/frc/:team', (req, res) => {
+    //return Array of JSONs where the team number is present
+    const teamNo = req.params.team;
+    async function getTeamObject() {
+        TeamMatch.find({ 'initials.team_id': teamNo }, (err, docs) => {
+            if (err) {
+                console.error ("ERROR: COULD NOT FETCH TEAM", err);
+                res.send("whoopsies");
+            } else {
+                res.send(docs);
+            }
+            //console.log("Error: " + err);
+            //console.log("DOCS: " + docs);
+        });
+    }
+    getTeamObject();
+});
+
+//We should also be able to fetch a team number AND match number:
+app.get('/frc/:team/:match', (req, res) => {
+    const teamNo = req.params.team;
+    const matchNo = req.params.match;
+    async function getTeamMatchObject() {
+        TeamMatch.find({'initials.team_id':teamNo, 'initials.match_no':matchNo}, (err, docs) => {
+            if (err) {
+                console.error("ERROR: CANNOT FETCH TEAM AND MATCH", err);
+                res.send(err);
+            } else {
+                res.send(docs);
+            }
+        });
+    }
+    getTeamMatchObject();
+});
+
+//Finally, just to test our POSTs
+app.post('/test', (req, res) => {
+    res.send(req);
 });
 
 //listen on a certain port. I chose 3000.
