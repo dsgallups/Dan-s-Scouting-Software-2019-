2278173d3796c9462b3ff7a3727bdc37ca893e8d
Unknown <dgallups@purdue.edu>
2019-03-05 14:44:18 -0500

    Added initial code for heat maps
    +Heat map design
    +Buggy script code that requires more fixing

--- Analytics/script.js
+++ Analytics/script.js
@@ -30,127 +30,271 @@ $(document).ready(function() {
 	//console.log("ready!");
 	//console.log(arr[10].initials.team_id);
 	
-var teams = [];
+let teams = [];
 
 $('.teams').on('input', function() {
-	console.log("input change");
-	teams = $('.teams').val().split(',');
-	for (var i = 0; i < teams.length; i++) {
-		console.log("teams["+i+"] = " + teams[i]);
-	};
-	
-	
-});
-function runVals() {
+	//console.log("input");
 	$('.container').html("");
-	$('.container').append("<div class=\"section-body\">");
-	for (var i = 0; i < teams.length; i++) {
+	teams = $('.teams').val().split(',');
+	
+	
+	//Build a schema for each team in our array
+	for (let i = 0; i < teams.length; i++) {
+		console.log("teams["+i+"] = " + teams[i]);
 		
-		//build our page
-		var teamID = teams[i];
+		//Clear up some confusion for myself
+		currentTeamNo = teams[i];
 		
-		$('.container').append("<div class=\"team-header\">Team #"+teamID+":</div>");
-		//$('.container').append("<div class=\"team-header\">Team #"+
-		//find the index in our array
-		var indices = [];
-		for (var j = 0; j < arr.length; j++) {
-			if (arr[j].initials.team_id == teamID) {
-				//console.log("team = " + arr[j].initials.team_id + ", j = " + j);
+		
+		//In order to do anything useful with our data, we're going to need to find the 
+		//indices of our gigantic array where data about our team is present
+		//So, let's build ourselves an array that contains the indices in our array for our data:
+		let indices = [];
+		for (let j = 0; j < arr.length; j++) {
+			//if our currentTeamNo matches the current object, push our indice:
+			if (currentTeamNo == arr[j].initials.team_id) {
 				indices.push(j);
 			}
 		}
-		//Sort our indices from most recent to least recent
-		indices.sort((a, b) => b - a);
+		//This is to check that we're getting this right:
+		/*
+		for (let j = 0; j < indices.length; j++) {
+			console.log("indices["+j+"] = " + indices[j]);
+		}
+		*/
 		
-		
-		//now we have indices, lets pull data
-		var runningTotalCargo = 0;
-		var runningTotalHatch = 0;
-		var runningTotalSandstorm = 0;
-		var runningTotalTeleop = 0;
-		
-		var runningTotalCargo3 = 0;
-		var runningTotalHatch3 = 0;
-		var runningTotalSandstorm3 = 0;
-		var runningTotalTeleop3 = 0;
-		
-		var noOfMatches = indices.length;
-		var noOfMatches3 = (noOfMatches < 3) ? noOfMatches : 3;	//the var to use when running avgs for last three matches
-		
-		
-		for (var k = 0; k < noOfMatches; k++) {
-			//console.log("indice for team 832 " + indices[k]);
-			//get avg points
+		//Next, we're going to want to grab all the points for all our current team's matches, and 
+		//push them to their respective arrays (remember, our points object don't tell whether its sandstorm or teleop).
+		var sandstormPoints = [];
+		var teleopPoints = [];
+		for (let j = 0; j < indices.length; j++) {
+			//So, arr[indices[j]] will point directly to each object that contains our team's data.
+			//Let's set that value to currentMatch (even though we're only recording the team from the match)
+			//just to clear up some confusion.
+			console.log("indices["+j+"] = "+indices[j]);
+			let currentMatch = arr[indices[j]];
 			
-			//sandstorm
-			//console.log("arr[indices[k]].sandstorm.points.length = " + arr[indices[k]].sandstorm.points.length);
-			for (var p = 0; p < arr[indices[k]].sandstorm.points.length; p++) {
-				//console.log("in loop");
-				runningTotalSandstorm++;
-				runningTotalCargo += (arr[indices[k]].sandstorm.points[p].cargo_type == "cargo") ? 1 : 0;
-			}
 			
-			//teleop
-			for (var p = 0; p < arr[indices[k]].teleop.points.length; p++) {
-				//console.log("in other loop");
-				runningTotalTeleop++;
-				runningTotalCargo += (arr[indices[k]].teleop.points[p].cargo_type == "cargo") ? 1 : 0;
+			//Now, the points are stored in an array in our object, so we'll need another for loop to grab those.
+			//According to our structure, points are stored in the sandstorm and teleop subobjects, so we'll need a for loop for both of them.
+			for (let k = 0; k < currentMatch.sandstorm.points.length; k++) {
+				sandstormPoints.push(currentMatch.sandstorm.points[k]);
 			}
-		}	
-		
-		//for last three matches
-		for (var k = 0; k < noOfMatches3; k++) {
-			for (var p = 0; p < arr[indices[k]].sandstorm.points.length; p++) {
-				//console.log("in loop");
-				runningTotalSandstorm3++;
-				runningTotalCargo3 += (arr[indices[k]].sandstorm.points[p].cargo_type == "cargo") ? 1 : 0;
-			}
-			
-			//teleop
-			for (var p = 0; p < arr[indices[k]].teleop.points.length; p++) {
-				//console.log("in other loop");
-				runningTotalTeleop3++;
-				runningTotalCargo3 += (arr[indices[k]].teleop.points[p].cargo_type == "cargo") ? 1 : 0;
+			//Let's do the same for teleop
+			for (let k = 0; k < currentMatch.teleop.points.length; k++) {
+				console.log("in loop, teleop = " + currentMatch.teleop.points[k]);
+				teleopPoints.push(currentMatch.teleop.points[k]);
 			}
 		}
-			
-			
+		console.log("here, teleopPoints(0) = " + teleopPoints[0]);
+		logArray(teleopPoints);
+		//WHEW! Now that we've finished that, let's make something reasonable out of all these points.
+		//I'm thinking a heatmap, and the functions are described below.
+		//I've built mapPoints(teamNo,sandstormPoints,teleopPoints), which returns a string containing the div to put on our heatmap
+		//NOTE: arrs passed to mapPoints should ONLY contain data points.
 		
+		let teamHeatMap = mapPoints(currentTeamNo,sandstormPoints,teleopPoints);
 		
-		$('.container').append("<div class=\"matches\">Matches recorded:" + noOfMatches +"</div>");
-	
-		$('.container').append("<div class=\"match-list\">In matches:");
-		for (var l = 0; l < indices.length; l++) {
-			$('.container').append(" <span>M=" + indices[l] + ";I="+l+"</span> ");
-		}
-		$('.container').append('</div>');
-		$('.container').append("<div class=\"running-total\">Running total (all points): " + (+runningTotalCargo + +runningTotalTeleop) + "</div>");
-		$('.container').append("<div class=\"subheader\">The following values are CUMULATIVE:</div>");
-		$('.container').append("<div class=\"avg-points\">avg points per match:" + Math.floor(((runningTotalCargo + runningTotalTeleop)/noOfMatches)*100)/100+"</div>");
-		$('.container').append("<div class=\"avg-cargo\">Cargo per match = " + Math.floor((runningTotalCargo/noOfMatches)*100)/100+"</div>");
-		$('.container').append("<div class=\"avg-hatch\">Hatch Panels per match: " + Math.floor((runningTotalHatch/noOfMatches)*100)/100 + "</div>");
-		$('.container').append("<div class=\"avg-sandstorm\">Average sandstorm period points: " + Math.floor((runningTotalCargo/noOfMatches)*100)/100+"</div>");
-		$('.container').append("<div class=\"avg-teleop\">avg teleop period points: " + Math.floor((runningTotalTeleop/noOfMatches)*100)/100+"</div>");
+		$('.container').append(teamHeatMap);
+		//For our current team,  we're going to want to build ourselves a heatmap:
 		
-		$('.container').append("<div class=\"subheader\">The following values are per the last three matches:</div>");
-		$('.container').append("<div class=\"avg-points-3\">avg points per match: " + Math.floor(((runningTotalCargo3 + runningTotalCargo3)/noOfMatches3)*100)/100+ "</div>");
-		$('.container').append("<div class=\"avg-cargo-3\">avg cargo per match = " + Math.floor((runningTotalCargo3/noOfMatches3)*100)/100 + "</div>");
-		$('.container').append("<div class=\"avg-hatch-3\">avg hatch panels per match = " + Math.floor((runningTotalHatch3/noOfMatches3)*100)/100 + "</div>");
-		$('.container').append("<div class=\"avg-sandstorm-3\">avg sandstorm period points = "+ Math.floor((runningTotalCargo3/noOfMatches3)*100)/100 + "</div>");
-		$('.container').append("<div class=\"avg-teleop-3\">avg teleop period points = "+ Math.floor((runningTotalTeleop3/noOfMatches3)*100)/100 + "</div>");
-		
-		//finally, list notes:
-		$('.container').append("<div class=\"notes\">");
-		$('.container').append("<ul>");
-		for (var l = 0; l < noOfMatches; l++) {
-			$('.container').append("<li>"+arr[indices[l]].notes+"</li>");
-		}
-		$('.container').append("</ul>");
-		$('.container').append("<div class=\"line\"></div>");
 		
 		
 		
 	}
-	$('.container').append("</div>");
-}
+
+	function mapPoints(teamNo, sandstormPoints, teleopPoints) {
+		/*
+			So I've been to the moon and back with function. Just to let you know, we'll have quite a few arrays to mess with:
+			lRocketSandstorm
+			lRocketTeleop
+			rRocketSandstorm
+			rRocketTeleop
+			cargoSandstorm
+			cargoTeleop
+			
+			Now I know that it seems unnecessary to divide each part by sandstorm and teleop, but my poor brain can't handle all of this. I really just want to take this apart piece by piece
+		*/
+		
+		
+		//These are important for our arrays. You'll notice these variables later.
+		const panel = 0;
+		const cargo = 1;
+		//First thing we need to do is to declare our div:
+		var div = "";
+		//Just to keep everything the same, I've initialized it blank. We'll add onto it.
+		div += "<div class=\"points-grid\">";
+			
+		//Here comes some confusion: Our rocket is rotate 90deg counter-clockwise. We'll have to account for that below
+		
+		//I'm thinking that for our columns, they'll be described like this:
+		//<tr class="<team no>-lrocket-<column no>">
+		
+		//Lets go ahead and build this thing
+		//So we'll want to build an array like this:
+		/*
+		var lRocket = [
+				CLOSE FAR
+		TOP		[[0,1], 	[1,1]],
+				[[3,1],		[5,4]],
+		BOTTOM	[[0,2], 	[1,0]]
+		
+		so lRocket[row][column][0=panel,1=cargo]
+		
+		*/
+		
+		//Get our arrays and store them
+		logArray(teleopPoints);
+		let lRocketSandstorm = buildMapArr(sandstormPoints, "lRocket");
+		let rRocketTeleop = buildMapArr(teleopPoints, "rRocket");
+		let rRocketSandstorm = buildMapArr(sandstormPoints, "rRocket");
+		let lRocketTeleop = buildMapArr(teleopPoints, "lRocket");
+		let cargoSandsotrm = buildMapArr(sandstormPoints, "cargo");
+		let cargoTeleop = buildMapArr(teleopPoints, "cargo");
+		function buildMapArr(points, name) {
+				try {
+				var newArr;
+				if (points.length == 3) {		
+					newArr = [
+						[[0,0], [0,0]],
+						[[0,0], [0,0]],
+						[[0,0], [0,0]]
+					];
+				} else if (points.length == 4) {
+					newArr = [
+						[[0,0], [0,0]],
+						[[0,0], [0,0]],
+						[[0,0], [0,0]],
+						[[0,0], [0,0]]
+					];
+				} else {
+					console.log("no map");
+					return null;
+				}
+				
+				for (let m = 0; m < sandstormPoints.length; m++) {
+					if (sandstormPoints[m].vehicle == name ) {
+						//this object actually has our col and row already, so we'll use them.
+						let row = points[m].row;
+						let col = points[m].col;
+						let type = points[m].cargo_type;
+						cargoSandstorm[row][col][panel] += (type == "panel") ? 1 : 0;
+						cargoSandstorm[row][col][cargo] += (type == "cargo") ? 1 : 0;			
+					}
+				}
+				
+				return newArr;
+				} catch (e) {
+					//return "
+				}
+		}
+		/*
+		let lRocketSandstorm = buildMapArr(sandstormPoints, "lRocket");
+		let rRocketTeleop = buildMapArr(teleopPoints, "rRocket");
+		let rRocketSandstorm = buildMapArr(sandstormPoints, "rRocket");
+		let lRocketTeleop = buildMapArr(teleopPoints, "lRocket");
+		let cargoSandsotrm = buildMapArr(sandstormPoints, "cargo");
+		let cargoTeleop = buildMapArr(teleopPoints, "cargo");
+		*/
+		//Append our new maps
+		div += buildSection(lRocketSandstorm, lRocketTeleop, "lRocket");
+		div += buildSection(cargoSandstorm, cargoTeleop, "cargo");
+		div += buildSection(rRocketSandstorm, rRocketTeleop, "rRocket");
+		
+		
+		//Now lets build the function to append these tables
+		function buildSection(sandstorm, teleop, type) {
+			var section = "";
+			var total;
+			if (sandstorm.length == 3 && teleop.length == 3) {
+				total = [
+					[[0,0], [0,0]],
+					[[0,0], [0,0]],
+					[[0,0], [0,0]]
+				];
+			} else if (sandstorm.length == 4 && teleop.length == 4) {
+				total = [
+					[[0,0], [0,0]],
+					[[0,0], [0,0]],
+					[[0,0], [0,0]],
+					[[0,0], [0,0]]
+				];
+			} else {
+				console.log("no map");
+				return null;
+			}
+			
+			
+			
+			for (let i = 0; i < sandstorm.length; i++) {
+				for (let j = 0; j < sandstorm[i].length; j++) {
+					for (let k = 0; k < sandstorm[i][j].length; k++) {
+						total[i][j][k] += sandstorm[i][j][k];
+						
+					}
+				}
+			}
+			for (let i = 0; i < teleop.length; i++) {
+				for (let j = 0; j < teleop[i].length; j++) {
+					for (let k = 0; k < teleop[i][j].length; k++) {
+						total[i][j][k] += teleop[i][j][k];
+					}
+				}
+			}	
+			console.log("log:");
+			logArray(total);
+			//get our max val to determine opacity
+			let maxVal = 0;
+			for (let i = 0; i < total.length; i++) {
+				for (let j = 0; j < total[i].length; j++) {
+					for (let k = 0; k < total[i][j].length; k++) {
+						maxVal = (total[i][j][k] > maxVal) ? total[i][j][k] : maxVal;
+					}
+				}
+			}			
+			
+			//Now lets use our total array
+			for (let i = 0; i < total[i].length; i++) {
+				section += "<tr class=\""+teamNo+"-"+type+"-"+i+"\">";
+				for (let j = 0; j < total.length; j++) {
+						currentSandstormPoints = total[j][i][0];
+						currentTeleopPoints = total[j][i][1];
+						section += "<th><div style=\"opacity:"+(+currentSandstormPoints+ +currentTeleopPoints)/maxVal+";\"></div></th>";
+						
+				}
+				section += "</tr>";
+			}
+			return section;
+		}
+		//close off the table:
+		div += "</div>";
+		return div;
+	} //mapPoints
+	
+
+	
+	//lets do debugging
+	function logArray(arr) {
+		if (arr[0][0].length) {
+			/*					
+			newArr = 
+			[
+				[[0,0], [0,0]],
+				[[0,0], [0,0]],
+				[[0,0], [0,0]]
+			];
+			*/
+			console.log("[");
+			for (let v = 0; v < arr.length; v++) {
+				console.log("["+arr[v][0][0]+","+arr[v][0][1]+"], ["+arr[v][1][0]+","+arr[v][1][1]+"]],");
+			}
+			console.log("]");
+		} else {
+			console.log("noarr");
+		}
+	}
+		
+
+});
+
 });
