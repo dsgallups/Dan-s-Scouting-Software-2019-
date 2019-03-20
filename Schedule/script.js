b1e65e859daa08b42ad7693491167d3b911a2bdc
Unknown <dgallups@purdue.edu>
2019-03-20 09:13:44 -0400

    Initial code for scheduling software

---
+++ Schedule/script.js
@@ -0,0 +1,55 @@
+"use strict";
+let scouters = ["Miguel","Travis","Mega Chris","Devin","Robin","Little Chris","Preston","Jashun","Rav","Amelia","Zach","Evie","Mason","Kam","Mason"];
+/*
+	Day 1 of Scouting goes from 10:30 AM-6 PM
+	Day 2 of Scouting goes from 10:30 AM-2 PM
+	
+	Intervals:	
+		10:30-11 AM
+		11 AM-12 PM
+		12 PM-1PM
+		1PM-2PM
+		...
+*/
+
+let positions;
+let currentIndex = 0;
+const noOfIntervals = 20;
+
+for (let i = 0; i < noOfIntervals; i++) {
+	
+}
+
+
+
+
+//Don't write below
+for (let i = 0; i < 6; i++) {
+	console.log(i);
+}
+/*
+	0
+	1
+	2
+	3
+	4
+	5
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
